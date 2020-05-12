ebd4e6847e446d3c27f1c93f1eb13bcb21ed53f1
davidparker1 <davidparker@flywheel.io>
2020-05-12 11:46:25 -0400

    ready to be a gear

---
+++ Dockerfile
@@ -0,0 +1,52 @@
+FROM ubuntu:bionic
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+RUN apt update && apt install -y software-properties-common \
+                                 wget \
+                                 zip \
+                                 cmake \
+                                 git \
+                                 zlib1g-dev \
+                                 graphviz \
+                                 libgraphviz-dev &&\
+    add-apt-repository -y ppa:deadsnakes/ppa && apt install -y python3.6 python3-pip && \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+# Install ANTS as described here 
+# https://github.com/ANTsX/ANTs/wiki/Compiling-ANTs-on-Linux-and-Mac-OS#superbuild-quick-reference
+ENV workingDir=/opt/ANTs
+
+RUN git clone https://github.com/ANTsX/ANTs.git && \
+    mkdir build install && \
+    cd build && \
+    cmake \
+        -DCMAKE_INSTALL_PREFIX=${workingDir}/install \
+        ../ANTs 2>&1 | tee cmake.log && \
+    make -j 4 2>&1 | tee build.log && \
+    cd ANTS-build && \
+    make install 2>&1 | tee install.log
+
+
+
+ENV ANTSPATH=/opt/ANTs/install/bin/
+ENV PATH=${ANTSPATH}:${PATH}
+
+RUN pip3 install nipype[all] && rm -rf /root/.cache/pip3
+
+COPY requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt && rm -rf /root/.cache/pip3
+
+RUN mkdir -p /flywheel/v0/templates
+
+ # Chosing these MNI spaces, learn more here:
+ # https://www.lead-dbs.org/about-the-mni-spaces/
+ 
+WORKDIR /flywheel/v0/templates
+
+# Download the 2009 asym MNI T1 Templates in zipped nifti format
+RUN wget http://www.bic.mni.mcgill.ca/~vfonov/icbm/2009/mni_icbm152_nlin_asym_09a_nifti.zip &&\
+    unzip mni_icbm152_nlin_asym_09a_nifti.zip && rm mni_icbm152_nlin_asym_09a_nifti.zip
+
+# Run Docker:
+# docker run --rm -it --entrypoint=/bin/bash -v /Users/davidparker/Documents/Flywheel/SSE/MyWork/Gears/nipype/DockerIO:/tmp flywheel/nipype:0.0.1_1.4.2
---
+++ run.py
@@ -0,0 +1,99 @@
+from nipype import Node, Function, Workflow
+import nipype.interfaces.ants as ants
+from nipype.interfaces.io import DataSink
+from pathlib import Path
+from ast import literal_eval
+import flywheel
+
+
+
+ # Instantiate a logger
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+log = logging.getLogger('root')
+
+def skullstrip_standard_node():
+    skullstrip_standard = Node(ants.MultiplyImages(
+        dimension=3,
+        first_input='/flywheel/v0/templates/mni_icbm152_nlin_asym_09a/mni_icbm152_t1_tal_nlin_asym_09a.nii',
+        second_input='/flywheel/v0/templates/mni_icbm152_nlin_asym_09a/mni_icbm152_t1_tal_nlin_asym_09a_mask.nii',
+        output_product_image='/flywheel/v0/templates/mni_icbm152_nlin_asym_09a/mni_icbm152_t1_tal_nlin_asym_09a_brain.nii'),
+        name='skullstrip_standard_node')
+
+    return (skullstrip_standard)
+
+
+def register_t1_2_standard_node(config):
+    reg = Node(ants.Registration(), name='registration_node')
+    
+    for key, val in reg.inputs.items():
+        
+        if key in config:
+            config_val = config[key]
+            
+            if isinstance(config_val,str):
+                config_val = ast.literal_eval(config_val)
+            
+            print('{}: {}'.format(key, config_val))
+            reg.set_input(key, config_val)
+    
+    log.debug(reg.inputs)
+    
+    return (reg)
+
+
+def make_workflow(input_t1, config, has_skull=True):
+    
+    workflow_directory = '/flywheel/v0/nipype/workflow'
+    Path(workflow_directory).mkdir(parents=True)
+    warped_output = 'warped_output.nii'
+    
+    standard = '/flywheel/v0/templates/mni_icbm152_nlin_asym_09a/mni_icbm152_t1_tal_nlin_asym_09a.nii'
+
+    wf = Workflow(name='register2standard', base_dir=workflow_directory)
+
+    registration_node = register_t1_2_standard_node(config)
+
+
+    registration_node.inputs.moving_image = input_t1
+    registration_node.inputs.fixed_image = standard
+    registration_node.inputs.output_warped_image = warped_output
+    
+    
+
+    if not has_skull:
+        skullstrip_node = skullstrip_standard_node()
+
+        wf.connect(skullstrip_node, "output_product_image", registration_node, "fixed_image")
+    else:
+        
+        registration_node.inputs.fixed_image = standard
+
+
+    sink = Node(DataSink(), name='sinker')
+
+    # Name of the output folder
+    sink.inputs.base_directory = '/flywheel/v0/nipype/sink'
+
+    wf.connect(registration_node, 'warped_image', sink, 'T1_Out')
+    
+    return (wf)
+
+        
+def main():
+    
+    
+    with flywheel.gear_context.GearContext() as context:
+    
+        config = context.config
+        has_skull = config['has-skull']
+        input_t1 = context.get_input_path('t1w-input')
+        
+        
+        wf = make_workflow(input_t1, config, has_skull=has_skull)
+    
+        wf.run()
+        
+
+
+if __name__ == "__main__":
+    main()
